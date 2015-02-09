0dcfcde4b51cbb20036c7f2f91eac08f361b9b57
Damien MOULIN <damien.moulin1@gmail.com>
2015-02-09 15:58:17 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,9 @@
-//Fichier de Connexion
 <?php
-	$db = new mysqli( 'localhost' , '' , '' , 'warcats' );
+//Fichier de Connexion
+
+	$db = new mysqli( 'localhost' , 'rootJ' , '' , 'warcats' );
 	if( $db->connect_errno ){
 		die( $db->connect_error );
 	}
+
+?>
